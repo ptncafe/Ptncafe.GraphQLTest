27846b929eef3eb5e5ce4c526b330b6293d32c64
nguyenpt8 <nguyenpt8@sendo.vn>
2019-04-10 17:12:34 +0700

    src\Ptncafe.GraphQLTest.Api\Startup.cs

--- src/Ptncafe.GraphQLTest.Api/Startup.cs
+++ src/Ptncafe.GraphQLTest.Api/Startup.cs
@@ -11,7 +11,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
-using Ptncafe.GraphQLTest.Api.QLGraphSchema;
+using Ptncafe.GraphQLTest.Api.GraphQLSchema;
 
 namespace Ptncafe.GraphQLTest.Api
 {
